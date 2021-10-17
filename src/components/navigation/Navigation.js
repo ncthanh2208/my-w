d1f07ce4d7869779c180e6aab1d3e1c034afd247
ncthanh2208 <nguyenchithanh228244@gmail.com>
2021-10-17 13:29:19 +0700

    update

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -24,15 +24,19 @@ const NavBar = () => {
           <nav className="flex flex-col justify-between md:flex-row">
             <div className="flex justify-center px-2 py-4">
               <div className="flex justify-between w-full">
-                <Link to="/" className="flex">
-                  <img src={logo} width="30" height="30" className="rounded-full cursor-pointer" alt="avatar" />
-                  <span className="flex flex-col py-4 px-4 text-yellow text-xl font-extrabold h-8 justify-center cursor-pointer">Thanh Nguyen</span>
-                </Link>
-              <BurgerButton isActive={isActive} toggle={toggleNav} /> 
+                <div className="flex">
+                  <Link to='/'>
+                    <img width="30" height="30" className="rounded-full cursor-pointer" src={logo} alt="avatar" />
+                  </Link>
+                  <Link to='/'>
+                    <span className="flex flex-col py-4 px-4 text-yellow text-xl h-8 justify-center font-extrabold">Thanh Nguyen</span>
+                  </Link>
+                </div>
+                <BurgerButton isActive={isActive} toggle={toggleNav} />
               </div>
             </div>
-            <div className="flex flex-col text-center justify-center font-semibold text-xl nav-items md:border-none md:flex-row md:max-h-300 py-4">
-            {/* <div className={`flex flex-col text-center justify-center font-semibold text-xl nav-items md:border-none md:flex-row md:max-h-300 py-4 ${!isActive ? 'show' : ''}`}> */}
+            <div className="flex flex-col text-center justify-center font-bold text-xl nav-items md:border-none md:flex-row py-4">
+              {/* <div className={`flex flex-col text-center justify-center font-semibold text-xl nav-items md:border-none md:flex-row md:max-h-300 py-4 ${!isActive ? 'show' : ''}`}> */}
               {navigation.map((item) => (
                 <ActiveLink href={item.href} onClick={closeNav}>
                   {item.name}
